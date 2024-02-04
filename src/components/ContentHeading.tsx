4e95cfec0d591695e55828c90ecc71a79486d0f7
nskreddy1 <naragantisunilkumar@gmail.com>
2024-02-05 01:18:45 +0530

    Change "Explore Movies" to "Explore Tv Shows" and vice versa in the ContentHeading component.

--- src/components/ContentHeading.tsx
+++ src/components/ContentHeading.tsx
@@ -6,7 +6,7 @@ interface props {
 const ContentHeading = ({ contentQuery }: props) => {
   const heading = contentQuery.Search
     ? `Search results of  ${contentQuery.Search}`
-    : `Explore ${contentQuery.Type == "movie" ? "Movies" : "Tv Shows"}`;
+    : `Explore ${contentQuery.Type == "tv" ? "Tv Shows": "Movies"  }`;
   return (
     <Box pb={4} ml={3}>
       <Heading as={"h1"} fontSize={'5xl'}>{heading}</Heading>
